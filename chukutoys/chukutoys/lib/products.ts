fa38bd2407594a3ab26e1701b40fd65790541e9d
ramparani <61373592+ramparani@users.noreply.github.com>
2026-02-07 23:14:04 +0530

    updated the get query
    updated the get query

--- chukutoys/chukutoys/lib/products.ts
+++ chukutoys/chukutoys/lib/products.ts
@@ -157,7 +157,7 @@ export function addProduct(input: {
 
 export async function getProducts() {  
   const productResponse = await callStrapiAPI(
-    `/products?populate=product_images&pagination[page]=1&pagination[pageSize]=100`,
+    `/products?populate=product_images&pagination[page]=1&pagination[pageSize]=200`,
     "Error fetching page",
     "GET"
   );
